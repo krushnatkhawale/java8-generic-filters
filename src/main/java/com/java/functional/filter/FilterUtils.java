7c9a6364a250b1f38a9ce6ab1f8eceae6fce3b07
krushnat <krushnatkhavale@gmail.com>
2017-05-28 19:16:33 +0530

    Doc Added

--- src/main/java/com/java/functional/filter/FilterUtils.java
+++ src/main/java/com/java/functional/filter/FilterUtils.java
@@ -7,11 +7,25 @@ import java.util.stream.Stream;
 public class FilterUtils {
 
 	
+	/**
+	 * @param stream
+	 * @param filters
+	 * @return filtered stream of Given type
+	 */
 	public static <T> Stream<T> getFilteredStream(Stream<T> stream, List<Predicate<T>> filters){
 		return filters.isEmpty() ? stream : applyFilters(stream, filters, filters.size()-1);
 	}
+	
+	
+	/**
+	 * @param stream
+	 * @param filters
+	 * @return filtered stream of Given type
+	 */
 	public static <T> Stream<T> applyFilters(Stream<T> stream, List<Predicate<T>> filters, int index){
-		return index>0 ? applyFilters(stream.filter(filters.get(index)), filters, index-1) : stream.filter(filters.get(index));
+		return index>ZERO ? applyFilters(stream.filter(filters.get(index)), filters, index-1) : stream.filter(filters.get(index));
 	}
 	
+	
+	final static int ZERO = 0;
 }
